218bc0e2c85e2c784431facd60f643b07bb509e2
eQuantum <8466209+eq19@users.noreply.github.com>
2024-08-19 01:48:49 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -24,7 +24,7 @@ git config --global user.email "${ACTOR}@users.noreply.github.com"
 CREDENTIAL=${TOKEN}
 [[ "${OWNER}" != "${USER}" ]] && CREDENTIAL=${INPUT_OWNER}
 REMOTE_REPO="https://${ACTOR}:${CREDENTIAL}@github.com/${OWNER}/$1.git"
-git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
+#git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
 #git add . && git commit -m "jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
 
 echo -e "\njob completed"
