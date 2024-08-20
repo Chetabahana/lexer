faf1fb9219255228f17291a8293fd32f01b0fd0c
eQuantum <8466209+eq19@users.noreply.github.com>
2024-08-21 04:31:36 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -6,30 +6,31 @@ hr='----------------------------------------------------------------------------
 echo -e "\n$hr\nFinal Space\n$hr"
 df -h
 
-if [ -d /mnt/disks/platform/usr/bin ]; then
+if [ !-d /mnt/disks/platform/usr/bin ]; then
   
+  HEADER="Accept: application/vnd.github+json"
+  echo ${GITHUB_ACCESS_TOKEN} | gh auth login --with-token
+  gh api -H "${HEADER}" rerun ${GITHUB_RUN_ID} --debug
+  
+else
+
   echo -e "\n$hr\nFinal Cloud\n$hr"
   /mnt/disks/platform/usr/bin/gcloud info
   
   echo -e "\n$hr\nFinal Network\n$hr"
   /mnt/disks/platform/usr/bin/docker network inspect bridge
 
-fi
+  # Set update workflow
+  git config --global user.name "${ACTOR}"
+  git config --global --add safe.directory ${GITHUB_WORKSPACE}
+  git config --global user.email "${ACTOR}@users.noreply.github.com"
 
-# Set update workflow
-git config --global user.name "${ACTOR}"
-git config --global --add safe.directory ${GITHUB_WORKSPACE}
-git config --global user.email "${ACTOR}@users.noreply.github.com"
+  CREDENTIAL=${TOKEN}
+  [[ "${OWNER}" != "${USER}" ]] && CREDENTIAL=${INPUT_OWNER}
+  REMOTE_REPO="https://${ACTOR}:${CREDENTIAL}@github.com/${OWNER}/$1.git"
+  #git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
+  #git add . && git commit -m "jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
 
-CREDENTIAL=${TOKEN}
-[[ "${OWNER}" != "${USER}" ]] && CREDENTIAL=${INPUT_OWNER}
-REMOTE_REPO="https://${ACTOR}:${CREDENTIAL}@github.com/${OWNER}/$1.git"
-#git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
-#git add . && git commit -m "jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
+  echo -e "\njob completed"
 
-echo -e "\njob completed"
-HEADER="Accept: application/vnd.github+json"
-echo ${GITHUB_ACCESS_TOKEN} | gh auth login --with-token
-TOTAL_COUNT=$(gh api -H "${HEADER}" /repos/${GITHUB_REPOSITORY}/actions/runners --jq '.total_count')
-RUNNER_ID=$(gh api -H "${HEADER}" /repos/${GITHUB_REPOSITORY}/actions/runners --jq '.runners.[].id')
-#if (( $TOTAL_COUNT != 0 )); then gh api --method DELETE -H "${HEADER}" /repos/${GITHUB_REPOSITORY}/actions/runners/${RUNNER_ID}; fi 
+fi 
