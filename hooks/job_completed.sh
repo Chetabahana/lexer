151caf75549cc20d9b3d4c4b5584525167fe6d4c
eQuantum <8466209+eq19@users.noreply.github.com>
2024-08-21 07:18:24 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -8,7 +8,6 @@ git config --global user.name "${GITHUB_ACTOR}"
 git config --global --add safe.directory ${GITHUB_WORKSPACE}
 git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
-
 if [ ! -d /mnt/disks/platform/usr/bin ]; then
   
   REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${GITHUB_REPOSITORY}.git"
