1460e3c223a493186f33f1c5498501210791e96e
eQuantum <8466209+eq19@users.noreply.github.com>
2024-08-26 20:48:48 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -19,7 +19,7 @@ git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
 REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
 cd /home/runner/_site && git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
-#git add . && git commit -m "action pages jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
+git add . && git commit -m "action pages jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
 
 echo -e "\njob completed"
 
