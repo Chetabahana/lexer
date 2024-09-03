1c310fe405e5d679cb1bf7072022926bdd57232a
eQuantum <8466209+eq19@users.noreply.github.com>
2024-09-03 19:49:39 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -15,12 +15,16 @@ if [ -d /mnt/disks/platform/usr/local/sbin ]; then
   /mnt/disks/platform/usr/bin/docker network inspect bridge
 
   echo -e "\n$hr\nNext Workflow\n$hr"
+  cd /home/runner/_site && touch .nojekyll
+
   git config --global user.name "${GITHUB_ACTOR}"
   git config --global --add safe.directory ${GITHUB_WORKSPACE}
   git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 
+  if [[ "${TARGET_REPOSITORY}" == "eq19/eq19.github.io" ]]; then echo "www.eq19.com" > CNAME; fi
   REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
-  cd /home/runner/_site && git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
+
+  git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
   git add . && git commit -m "action pages jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
 
   echo -e "\njob completed"
