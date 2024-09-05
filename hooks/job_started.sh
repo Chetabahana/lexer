28a6cf213c4aac6fe3083d960be9be9360fc85dc
eQuantum <8466209+eq19@users.noreply.github.com>
2024-09-05 07:43:03 +0700

    Update job_started.sh

--- hooks/job_started.sh
+++ hooks/job_started.sh
@@ -22,7 +22,6 @@ echo -e "\n$hr\nDisk Structure\n$hr"
 df -h
 
 echo -e "\n$hr\nRunner 🏠 dir\n$hr"
-cat /home/runner/_site/.env
 set -a && . /home/runner/_site/.env && set +a
 ls -al /home/runner && ls -al /home/runner/_site
 
