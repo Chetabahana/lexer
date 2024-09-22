30430539fcef58d6561d8896919d91f0efd48e85
eQuantum <8466209+eq19@users.noreply.github.com>
2024-09-22 10:38:42 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -21,12 +21,12 @@ if [ -d /mnt/disks/platform/usr/local/sbin ]; then
   #echo -e "\n$hr\nBUILD\n$hr"
   #Jekyll Quick Reference https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
   #JEKYLL_GITHUB_TOKEN=${GITHUB_ACCESS_TOKEN} DISABLE_WHITELIST=true jekyll build --profile -t -p /home/runner/_site/_plugins -d ${GITHUB_WORKSPACE}/_site
-  #git config --global --add safe.directory ${GITHUB_WORKSPACE}/site && cd ${GITHUB_WORKSPACE}/_site && touch .nojekyll
+  #git config --global --add safe.directory "${GITHUB_WORKSPACE}/site" && cd ${GITHUB_WORKSPACE}/_site && touch .nojekyll
 
   if [[ "${TARGET_REPOSITORY}" == "eq19/eq19.github.io" ]]; then echo "www.eq19.com" > CNAME; fi
   REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
 
-  git config --global user.name "${GITHUB_ACTOR}
+  git config --global user.name "${GITHUB_ACTOR}"
   git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
   rm -rf .git && git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
   git add . && git commit -m "action pages jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
