e0b42cf9d06d14ea1c88116afa1afebb5cfaff38
eQuantum <8466209+eq19@users.noreply.github.com>
2024-09-22 15:42:23 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -11,9 +11,6 @@ if [ -d /mnt/disks/platform/usr/local/sbin ]; then
   echo -e "\n$hr\nDocker images\n$hr"
   /mnt/disks/platform/usr/bin/docker image ls
 
-  echo -e "\n$hr\nFinal Network\n$hr"
-  /mnt/disks/platform/usr/bin/docker network inspect bridge
-
   echo -e "\n$hr\nBuild Pages\n$hr"
   cd /home/runner/_site && rm -rf .github
   set -a && . /home/runner/_site/.env && set +a
@@ -21,10 +18,12 @@ if [ -d /mnt/disks/platform/usr/local/sbin ]; then
   #Jekyll Quick Reference https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
   JEKYLL_GITHUB_TOKEN=${GITHUB_ACCESS_TOKEN} DISABLE_WHITELIST=true jekyll build --profile -t -p /home/runner/_site/_plugins -d /home/runner/_site/docs
   
-  echo -e "\n$hr\nNext Workflow\n$hr"
+  echo -e "\n$hr\nFinal Network\n$hr"
   touch /home/runner/_site/docs/.nojekyll
+  /mnt/disks/platform/usr/bin/docker network inspect bridge
   if [[ "${TARGET_REPOSITORY}" == "eq19/eq19.github.io" ]]; then echo "www.eq19.com" > /home/runner/_site/docs/CNAME; fi
 
+  echo -e "\n$hr\nNext Workflow\n$hr"
   git config --global user.name "${GITHUB_ACTOR}"
   git config --global --add safe.directory "/home/runner/_site"
   git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
