edfc5c8a2c609351cc257186432ff50b02f2d313
eQuantum <8466209+eq19@users.noreply.github.com>
2024-09-23 15:44:13 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -17,7 +17,6 @@ if [ -d /mnt/disks/platform/usr/local/sbin ]; then
 
   #Jekyll Quick Reference https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
   REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
-  rm -rf .git && git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
   JEKYLL_GITHUB_TOKEN=${GITHUB_ACCESS_TOKEN} DISABLE_WHITELIST=true jekyll build --profile -t -p /home/runner/_site/_plugins -d /home/runner/_site/docs
   
   echo -e "\n$hr\nFinal Network\n$hr"
@@ -29,6 +28,7 @@ if [ -d /mnt/disks/platform/usr/local/sbin ]; then
   git config --global user.name "${GITHUB_ACTOR}"
   git config --global --add safe.directory "/home/runner/_site"
   git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+  #rm -rf .git && git init --initial-branch=master > /dev/null && git remote add origin ${REMOTE_REPO}
   git add . && git commit -m "action pages jekyll build" > /dev/null && git push --force ${REMOTE_REPO} master:gh-pages
 
   echo -e "\njob completed"
