b40c433468b9992c6e78defb0902fff932a37f9b
eQuantum <8466209+eq19@users.noreply.github.com>
2024-10-06 11:43:24 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -8,17 +8,14 @@ df -h
 
 if [ -d /mnt/disks/platform/usr/local/sbin ]; then
 
-  cd /home/runner/_site && rm -rf .github
+  cd /home/runner/_site
   set -a && . /home/runner/_site/.env && set +a
 
   git config --global user.name "${GITHUB_ACTOR}"
   git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+
   REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
   git remote set-url origin ${REMOTE_REPO} && git config --global --add safe.directory "/home/runner/_site"
-  
-  echo -e "\n$hr\nBuilded Pages\n$hr"
-  if [[ "${TARGET_REPOSITORY}" == "eq19/eq19.github.io" ]]; then echo "www.eq19.com" > /home/runner/_site/docs/CNAME; fi
-  touch /home/runner/_site/docs/.nojekyll && ls -al /home/runner/_site/docs
 
   echo -e "\n$hr\nDocker images\n$hr"
   /mnt/disks/platform/usr/bin/docker image ls
@@ -26,14 +23,23 @@ if [ -d /mnt/disks/platform/usr/local/sbin ]; then
   echo -e "\n$hr\nFinal Network\n$hr"
   /mnt/disks/platform/usr/bin/docker network inspect bridge
 
-  echo -e "\n$hr\nNext Workflow\n$hr"
+  echo -e "\n$hr\nBuilded Pages\n$hr"
   git add . && TEST_COMMIT=$(git commit -m "${LATEST_COMMIT}")
 
   if [[ "${TEST_COMMIT}" =~ "nothing to commit" ]] ; then
+    ls -al /home/runner/_site/docs
+
+    echo -e "\n$hr\nNext Workflow\n$hr"
     git commit --allow-empty -m "${LATEST_COMMIT}" && git push
   else
     #Jekyll Quick Reference https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
     JEKYLL_GITHUB_TOKEN=${GITHUB_ACCESS_TOKEN} DISABLE_WHITELIST=true jekyll build --profile -t -p /home/runner/_site/_plugins -d /home/runner/_site/docs    
+
+    if [[ "${TARGET_REPOSITORY}" == "eq19/eq19.github.io" ]]; then echo "www.eq19.com" > /home/runner/_site/docs/CNAME; fi
+    rm -rf /home/runner/_site/docs/.nojekyll && touch /home/runner/_site/docs/.nojekyll
+    ls -al /home/runner/_site/docs
+
+    echo -e "\n$hr\nNext Workflow\n$hr"
     git add . && git commit -m "${LATEST_COMMIT}" && git push
   fi
 
