0d43061de9c3d2567dca301c5eaf6007945e21b4
eQuantum <8466209+eq19@users.noreply.github.com>
2024-10-06 11:01:16 +0700

    Update job_completed.sh

--- hooks/job_completed.sh
+++ hooks/job_completed.sh
@@ -7,14 +7,13 @@ echo -e "\n$hr\nFinal Space\n$hr"
 df -h
 
 if [ -d /mnt/disks/platform/usr/local/sbin ]; then
-    
+
   cd /home/runner/_site && rm -rf .github
   set -a && . /home/runner/_site/.env && set +a
 
-  #Jekyll Quick Reference https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
-  REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
+  git config --global user.name "${GITHUB_ACTOR}"
+  git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"  REMOTE_REPO="https://${GITHUB_ACTOR}:${GITHUB_ACCESS_TOKEN}@github.com/${TARGET_REPOSITORY}.git"
   git remote set-url origin ${REMOTE_REPO} && git config --global --add safe.directory "/home/runner/_site"
-  #JEKYLL_GITHUB_TOKEN=${GITHUB_ACCESS_TOKEN} DISABLE_WHITELIST=true jekyll build --profile -t -p /home/runner/_site/_plugins -d /home/runner/_site/docs
   
   echo -e "\n$hr\nBuilded Pages\n$hr"
   if [[ "${TARGET_REPOSITORY}" == "eq19/eq19.github.io" ]]; then echo "www.eq19.com" > /home/runner/_site/docs/CNAME; fi
@@ -27,9 +26,14 @@ if [ -d /mnt/disks/platform/usr/local/sbin ]; then
   /mnt/disks/platform/usr/bin/docker network inspect bridge
 
   echo -e "\n$hr\nNext Workflow\n$hr"
-  git config --global user.name "${GITHUB_ACTOR}"
-  git config --global user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-  git add . && git commit --allow-empty -m "${LATEST_COMMIT}" > /dev/null && git push
+  git add . && TEST_COMMIT=$(git commit -m "${LATEST_COMMIT}")
+  if [[ "${TEST_COMMIT}"=="nothing to commit" ]] ; then
+    git commit --allow-empty -m "${LATEST_COMMIT}" && git push
+  else
+    #Jekyll Quick Reference https://gist.github.com/DrOctogon/bfb6e392aa5654c63d12
+    JEKYLL_GITHUB_TOKEN=${GITHUB_ACCESS_TOKEN} DISABLE_WHITELIST=true jekyll build --profile -t -p /home/runner/_site/_plugins -d /home/runner/_site/docs    
+    git add . && git commit -m "${LATEST_COMMIT}" && git push
+  fi
 
   if [ $? -eq 0 ]; then
     echo -e "\njob completed"
